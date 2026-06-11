715373655db4f2b04b71c4811ae50c9c2033fe5e
gle6chik <glebbaranov21@gmail.com>
2026-06-11 16:28:36 +0700

    feat: make coastline more visible with thicker pen. Closes #9

--- src/ui/GridScene.cpp
+++ src/ui/GridScene.cpp
@@ -560,7 +560,7 @@ void GridScene::setCoastlineCells(const QVector<QPointF> &cells) {
     }
 
     QBrush brush(Qt::red);
-    QPen pen(Qt::NoPen);
+    QPen pen(Qt::red, 1, Qt::SolidLine);
 
     for (const QPointF& cell : cells) {
         const qreal CELL_SIDE_SIZE = 1.0;
